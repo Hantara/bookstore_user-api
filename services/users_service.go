48a05e271d673cf6b82f28a88d4a80a46f003fac
Hantara <angga.hantara@gmail.com>
2020-04-09 16:16:12 +0700

    fix services problem

--- services/users_service.go
+++ services/users_service.go
@@ -1,12 +1,10 @@
 package services
 
 import (
-	"os/user"
-
 	"github.com/Hantara/bookstore_user-api/domain/users"
 )
 
 //CreateUser = Business logic pembuatan user
-func CreateUser(user users.User) (*user.User, error) {
+func CreateUser(user users.User) (*users.User, error) {
 	return &user, nil
 }
